65ab79d025770c560cfe3836ceeceeb0df5c821c
sk-fahim-hossain <fahimhossainjob1@gmail.com>
2021-12-04 15:07:39 +0600

    disgused between null and undifined

---
+++ null-vs-undefined.js
@@ -0,0 +1,28 @@
+//each code run individually
+
+let pakhi;
+console.log(pakhi);
+//> undefined;
+
+function add(num1,num2){
+    
+}
+const result = add(2,5);
+console.log(result);
+//> undefined;
+
+function add(num1,num2){
+    return   
+}
+const result = add(2,5);
+console.log(result);
+//> undefined;
+
+function add(num1,num2){
+    
+}
+const result = add(2);
+console.log(result);
+//> undefined;
+
+
